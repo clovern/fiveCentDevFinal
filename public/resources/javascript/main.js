73df60765c1375c8caccea8aca399a8d9137f5fa
clovern <56206787+clovern@users.noreply.github.com>
2021-04-08 09:15:55 -0400

    Update main.js

--- public/resources/javascript/main.js
+++ public/resources/javascript/main.js
@@ -8,7 +8,7 @@ function addProject(image, name, dateCompleted, languagesUsed, librariesUsed, de
     "librariesUsed": librariesUsed, "description": description, "sourceCodeLink": sourceCodeLink})
     }
 
-addProject("./resources/javascript/jImages/Euchre.png", "Euchre", "April 2020", "Python", "Tkinter", "Euchre game against 3 AI players", "./euchre.html");
+addProject("./resources/javascript/jImages/Euchre.png", "Euchre", "April 2020", "Python", "Tkinter", "Euchre game against 3 AI players", "./Euchre.html");
 addProject("./resources/javascript/jImages/wildChefMadness.png", "Wild Chef Madness", "January 2020", "Python", "", "Text branching game simulating a cooking show", "wildChefMadness.html");
 addProject("./resources/javascript/jImages/pokeStats.png", "Pokemon Stats Data Analysis", "April 2020", "Python", "iPywidgets (Jupyter notebook), \
 matplotlib, pandas", "retrieves useful information such as stat spread, strongest pokemon per type, \nstrengths and weaknesses \
