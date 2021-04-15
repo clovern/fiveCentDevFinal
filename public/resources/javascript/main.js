993279006db55a8d13f9ec7baa0935c096de302e
clovern <56206787+clovern@users.noreply.github.com>
2021-04-14 23:38:02 -0400

    Update main.js

--- public/resources/javascript/main.js
+++ public/resources/javascript/main.js
@@ -15,7 +15,7 @@ function addProject(image, name, dateCompleted, languagesUsed, librariesUsed, de
 // per pokemon in a table format", "./pokeStats.html");
 addProject("./resources/javascript/jImages/rentalStoreGUIPic.png", "Game Rental Store Interface", "March 2021", "Java", "", "Interface to help game rental store owner track information about customer rentals", "./gameRental.html");
 addProject("./resources/javascript/jImages/gladiatorPic.png", "Gladiator Game", "April 2021", "Java", "", "Survival text game implementing queus of monsters attacking from each cardinal direction", "./gladiator.html");
-addProject("./resources/javascript/jImages/stopWatchPic.png", "Triple Stopwatch GUI", "February 2021", "Java", "", "Simple GUI running and saving/loading multiple stopwatches with various settings", "./stopwatch.html");
+addProject("./resources/javascript/jImages/stopWatchPic.png", "Triple Stopwatch GUI", "February 2021", "Java", "", "Simple GUI running and saving/loading multiple stopwatches with various settings", "./stopWatch.html");
 addProject("./resources/javascript/jImages/fiveCentDevPic.png", "FiveCentDev Website", "December 2020", "JavaScript, HTML, CSS, Express (Node.js)", "", "This website! Clicking this banner will route you to the code on github.", "https://github.com/clovern/fiveCentDevFinal");
 addProject("./resources/javascript/jImages/Euchre.png", "Euchre", "April 2020", "Java", "Tkinter", "Euchre game against 3 AI players", "./Euchre.html");
 addProject("./resources/javascript/jImages/wildChefMadness.png", "Wild Chef Madness", "January 2020", "Python", "", "Text branching game simulating a cooking show", "wildChefMadness.html");
